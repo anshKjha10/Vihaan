2b813ed3d9e854de94ddff696130a58a326c9d2f
anshKjha10 <149003758+anshKjha10@users.noreply.github.com>
2025-04-14 18:16:15 +0530

    Update Financial_Advisor.py

--- Financial_Advisor.py
+++ Financial_Advisor.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-df = pd.read_csv("C:\\Users\\anshj\\Downloads\\indian_financial_advisors_extended.csv") 
+df = pd.read_csv("C:\\Finance-Advisor\\indian_financial_advisors_extended.csv") 
 
 
 st.set_page_config(page_title="Financial Advisor Recommender", layout="centered")
